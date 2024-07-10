05c6aa10957923ba2aeec1e6e113f53ae9f862c3
Enes <47952155+enessimsek159@users.noreply.github.com>
2024-07-10 14:02:03 +0300

    Initial Commit

---
+++ WhatsappCloneExample/WhatsappCloneExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCloneExampleApp.swift
+//  WhatsappCloneExample
+//
+//  Created by Mustafa Şimşek on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCloneExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
